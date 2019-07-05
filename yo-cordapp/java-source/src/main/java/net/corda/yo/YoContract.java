246b8569c1a2d3b68249d22eec3912b52113b17e
FreyaRhianna <freyarsh@gmail.com>
2019-07-05 12:03:49 +0100

    Added conract and state

---
+++ yo-cordapp/java-source/src/main/java/net/corda/yo/YoContract.java
@@ -0,0 +1,34 @@
+package net.corda.yo;
+
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.CommandWithParties;
+import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.TypeOnlyCommandData;
+import net.corda.core.transactions.LedgerTransaction;
+
+import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+
+import org.jetbrains.annotations.NotNull;
+
+public class YoContract implements Contract {
+    public static String ID = "net.corda.yo.YoContract";
+
+    @Override
+    public void verify(@NotNull LedgerTransaction tx) throws IllegalArgumentException {
+        final CommandWithParties<Commands.Send> command = requireSingleCommand(tx.getCommands(), Commands.Send.class);
+        requireThat(require -> {
+            require.using("There can be no inputs when Yo'ing other parties.", tx.getInputs().isEmpty());
+            require.using("There must be one output: The Yo!", tx.getOutputs().size() == 1);
+            final YoState yo = tx.outputsOfType(YoState.class).get(0);
+            require.using("No sending Yo's to yourself!", yo.getTarget() != yo.getOrigin());
+            require.using("The Yo! must be signed by the sender", yo.getOrigin().getOwningKey() == command.getSigners().get(0));
+            return null;
+        });
+    }
+
+
+    public interface Commands extends CommandData {
+        class Send extends TypeOnlyCommandData {}
+    }
+}
--- yo-cordapp/java-source/src/main/java/net/corda/yo/YoState.java
+++ yo-cordapp/java-source/src/main/java/net/corda/yo/YoState.java
@@ -1,5 +1,61 @@
 package net.corda.yo;
 
-public class YoState{
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
+public class YoState implements ContractState {
+    private Party origin;
+    private Party target;
+    private String yo;
+
+    public Party getOrigin() {
+        return origin;
+    }
+
+    public Party getTarget() {
+        return target;
+    }
+
+    public String getYo() {
+        return yo;
+    }
+
+    YoState(Party origin, Party target, String yo){
+        this.origin = origin;
+        this.target = target;
+        this.yo = yo;
+    }
+
+    YoState(Party origin, Party target){
+        this(origin, target, "Yo");
+    }
+
+    @Override
+    public String toString(){
+        return origin.nameOrNull() + "yo";
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return null;
+    }
 }
+
+
+/*
+
+
+// State.
+@BelongsToContract(YoContract::class)
+data class YoState(val origin: Party,
+                 val target: Party,
+                 val yo: String = "Yo!") : ContractState {
+    override val participants = listOf(target)
+    override fun toString() = "${origin.name}: $yo"
+}
+ */
