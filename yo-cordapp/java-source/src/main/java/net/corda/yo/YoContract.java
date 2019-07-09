85cb7f478bd852732ae8d6412df8912b52c3ea6e
FreyaRhianna <freyarsh@gmail.com>
2019-07-09 09:51:43 +0100

    Finished tests

--- yo-cordapp/java-source/src/main/java/net/corda/yo/YoContract.java
+++ yo-cordapp/java-source/src/main/java/net/corda/yo/YoContract.java
@@ -16,15 +16,19 @@ public class YoContract implements Contract {
 
     @Override
     public void verify(@NotNull LedgerTransaction tx) throws IllegalArgumentException {
-        /*final CommandWithParties<Commands.Send> command = requireSingleCommand(tx.getCommands(), Commands.Send.class);
+        final CommandWithParties<Commands.Send> command = requireSingleCommand(tx.getCommands(), Commands.Send.class);
         requireThat(require -> {
             require.using("There can be no inputs when Yo'ing other parties.", tx.getInputs().isEmpty());
             require.using("There must be one output: The Yo!", tx.getOutputs().size() == 1);
             final YoState yo = tx.outputsOfType(YoState.class).get(0);
             require.using("No sending Yo's to yourself!", yo.getTarget() != yo.getOrigin());
-            require.using("The Yo! must be signed by the sender", yo.getOrigin().getOwningKey() == command.getSigners().get(0));
+            System.out.println("OWNING KEY");
+            System.out.println(yo.getOrigin().getOwningKey());
+            System.out.println("signer KEY");
+            System.out.println(command.getSigners().get(0));
+            require.using("The Yo! must be signed by the sender.", yo.getOrigin().getOwningKey().equals(command.getSigners().get(0)));
             return null;
-        });*/
+        });
     }
 
 
