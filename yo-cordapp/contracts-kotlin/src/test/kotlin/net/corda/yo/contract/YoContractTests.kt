ac7115e207cc6c3783a30915f52c07bf36e512d0
FreyaRhianna <freyarsh@gmail.com>
2019-07-10 09:48:16 +0100

    Updated file structure to match V4 recommended

--- yo-cordapp/contracts-kotlin/src/test/kotlin/net/corda/yo/contract/YoContractTests.kt
+++ yo-cordapp/contracts-kotlin/src/test/kotlin/net/corda/yo/contract/YoContractTests.kt
@@ -1,60 +1,20 @@
-package net.corda.yo
+package net.corda.yo.test.contract
 
 import net.corda.core.identity.CordaX500Name
-import net.corda.core.node.services.queryBy
-import net.corda.core.utilities.getOrThrow
 import net.corda.testing.contracts.DummyContract
 import net.corda.testing.contracts.DummyState
 import net.corda.testing.core.DummyCommandData
 import net.corda.testing.core.TestIdentity
-import net.corda.testing.node.*
-import org.junit.After
-import org.junit.Before
+import net.corda.testing.node.MockServices
+import net.corda.testing.node.ledger
+import net.corda.yo.contract.YoContract
+import net.corda.yo.state.YoState
 import org.junit.Test
-import kotlin.test.assertEquals
 
-class YoFlowTests {
-    private lateinit var network: MockNetwork
-    private lateinit var a: StartedMockNode
-    private lateinit var b: StartedMockNode
 
-    @Before
-    fun setup() {
-        network = MockNetwork(MockNetworkParameters(cordappsForAllNodes = listOf(
-                TestCordapp.findCordapp("net.corda.yo"))))
-        a = network.createPartyNode()
-        b = network.createPartyNode()
-        network.runNetwork()
-    }
-
-    @After
-    fun tearDown() {
-        network.stopNodes()
-    }
-
-    @Test
-    fun flowWorksCorrectly() {
-        val yo = YoState(a.info.legalIdentities.first(), b.info.legalIdentities.first())
-        val flow = YoFlow(b.info.legalIdentities.first())
-        val future = a.startFlow(flow)
-        network.runNetwork()
-        val stx = future.getOrThrow()
-        // Check yo transaction is stored in the storage service.
-        val bTx = b.services.validatedTransactions.getTransaction(stx.id)
-        assertEquals(bTx, stx)
-        println("bTx == $stx")
-        // Check yo state is stored in the vault.
-        b.transaction {
-            // Simple query.
-            val bYo = b.services.vaultService.queryBy<YoState>().states.single().state.data
-            assertEquals(bYo.toString(), yo.toString())
-            println("$bYo == $yo")
-        }
-    }
-}
 
 class YoContractTests {
-    private val ledgerServices = MockServices(listOf("net.corda.yo", "net.corda.testing.contracts"))
+    private val ledgerServices = MockServices(listOf("net.corda.yo.contract", "net.corda.yo.flow", "net.corda.testing.contracts"))
     private val alice = TestIdentity(CordaX500Name("Alice", "New York", "US"))
     private val bob = TestIdentity(CordaX500Name("Bob", "Tokyo", "JP"))
     private val miniCorp = TestIdentity(CordaX500Name("MiniCorp", "New York", "US"))
--- yo-cordapp/workflows-kotlin/src/main/kotlin/net.corda.yo.flow/YoFlow.kt
+++ yo-cordapp/workflows-kotlin/src/main/kotlin/net.corda.yo.flow/YoFlow.kt
@@ -1,13 +1,15 @@
-package net.corda.yo
+package net.corda.yo.flow
 
 import co.paralleluniverse.fibers.Suspendable
-import net.corda.core.contracts.*
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.StateAndContract
 import net.corda.core.flows.*
 import net.corda.core.identity.Party
-import net.corda.core.transactions.LedgerTransaction
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
+import net.corda.yo.contract.YoContract
+import net.corda.yo.state.YoState
 
 // Flow.
 @InitiatingFlow
@@ -56,34 +58,4 @@ class YoFlowResponder(val counterpartySession: FlowSession) : FlowLogic<SignedTr
     override fun call(): SignedTransaction {
         return subFlow(ReceiveFinalityFlow(counterpartySession))
     }
-}
-
-// Contract and state.
-class YoContract: Contract {
-    companion object {
-        // Used to identify our contract when building a transaction.
-        const val ID = "net.corda.yo.YoContract"
-    }
-
-    // Command.
-    class Send : TypeOnlyCommandData()
-
-    // Contract code.
-    override fun verify(tx: LedgerTransaction) = requireThat {
-        val command = tx.commands.requireSingleCommand<Send>()
-        "There can be no inputs when Yo'ing other parties." using (tx.inputs.isEmpty())
-        "There must be one output: The Yo!" using (tx.outputs.size == 1)
-        val yo = tx.outputsOfType<YoState>().single()
-        "No sending Yo's to yourself!" using (yo.target != yo.origin)
-        "The Yo! must be signed by the sender." using (yo.origin.owningKey == command.signers.single())
-    }
-}
-
-// State.
-@BelongsToContract(YoContract::class)
-data class YoState(val origin: Party,
-                 val target: Party,
-                 val yo: String = "Yo!") : ContractState {
-    override val participants = listOf(target)
-    override fun toString() = "${origin.name}: $yo"
 }
