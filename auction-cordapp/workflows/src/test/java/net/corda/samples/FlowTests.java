66a982819c043aa165234ad107976c33d310b53e
ashutoshmeher-r3 <ashutosh.meher@r3.com>
2020-03-19 12:59:59 +0530

    Mock Network mPV updated to 4

--- auction-cordapp/workflows/src/test/java/net/corda/samples/FlowTests.java
+++ auction-cordapp/workflows/src/test/java/net/corda/samples/FlowTests.java
@@ -3,6 +3,7 @@ package net.corda.samples;
 import com.google.common.collect.ImmutableList;
 import net.corda.core.concurrent.CordaFuture;
 import net.corda.core.contracts.Amount;
+import net.corda.core.node.NetworkParameters;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.samples.flows.BidFlow;
 import net.corda.samples.flows.CreateAssetFlow;
@@ -17,9 +18,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Collections;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
@@ -37,7 +40,9 @@ public class FlowTests {
                             TestCordapp.findCordapp("net.corda.samples.flows"),
                             TestCordapp.findCordapp("net.corda.samples.contracts")
                         )
-                )
+                ).withNetworkParameters(new NetworkParameters(4, Collections.emptyList(),
+                        10485760, 10485760 * 50, Instant.now(), 1,
+                        Collections.emptyMap()))
         );
         a = network.createPartyNode(null);
         b = network.createPartyNode(null);
@@ -51,8 +56,9 @@ public class FlowTests {
 
     @Test
     public void testCreateAssetFlow() throws Exception {
-        CreateAssetFlow flow = new CreateAssetFlow("Test Asset", "Dummy Asser", "http://abc.com/dummy.png");
+        CreateAssetFlow flow = new CreateAssetFlow("Test Asset", "Dummy Asset", "http://abc.com/dummy.png");
         CordaFuture<SignedTransaction> future = a.startFlow(flow);
+        network.runNetwork();
         SignedTransaction signedTransaction = future.get();
         Asset asset = (Asset) signedTransaction.getTx().getOutput(0);
         assertNotNull(asset);
@@ -60,15 +66,17 @@ public class FlowTests {
 
     @Test
     public void testCreateAuctionFlow() throws Exception {
-        CreateAssetFlow assetflow = new CreateAssetFlow("Test Asset", "Dummy Asser", "http://abc.com/dummy.png");
+        CreateAssetFlow assetflow = new CreateAssetFlow("Test Asset", "Dummy Asset", "http://abc.com/dummy.png");
         CordaFuture<SignedTransaction> future = a.startFlow(assetflow);
+        network.runNetwork();
         SignedTransaction signedTransaction = future.get();
         Asset asset = (Asset) signedTransaction.getTx().getOutput(0);
         CreateAuctionFlow.Initiator auctionFlow = new CreateAuctionFlow.Initiator(Amount.parseCurrency("1000 USD"),
                 asset.getLinearId().getId(), LocalDateTime.ofInstant(Instant.now().plusMillis(30000), ZoneId.systemDefault()));
         CordaFuture<SignedTransaction> future1 = a.startFlow(auctionFlow);
+        network.runNetwork();
         SignedTransaction transaction = future1.get();
         AuctionState auctionState = (AuctionState) transaction.getTx().getOutput(0);
-        assertNotNull(auctionFlow);
+        assertNotNull(auctionState);
     }
 }
